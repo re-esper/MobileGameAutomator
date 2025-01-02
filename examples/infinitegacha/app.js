a9d035c29ac694984095b612600b183520b95542
re-esper <bin.esper@gmail.com>
2025-01-02 22:28:08 +0800

    add version 2.0 examples

---
+++ examples/infinitegacha/app.js
@@ -0,0 +1,29 @@
+require('../../engine/engine');
+
+const TARGET_SSR = 3;
+
+(async () => {
+    await InitEngine(1920, 1080);
+    console.log("engine initialized");
+	
+	while (true)
+	{
+		let p;
+		while (!(p = Find("reroll.png")))
+		{
+			await Touch(1734, 70);
+			await Sleep(0.25);			
+		}
+
+		const r = FindAll("5stars.png");
+		console.log(`ssr count: ${r.length}`);
+		if (r.length >= TARGET_SSR) break;
+
+		await Touch(p.x, p.y);
+
+		await TouchEx("okey.png", { retry: true, delay: 0.5 });
+	}
+
+	console.log("done");
+
+})();
---
+++ examples/refinepowder/app.js
@@ -0,0 +1,41 @@
+const bot = require('../../engine/engine');
+
+const ROUND = 45;
+
+(async () => {
+    await InitEngine(1920, 1080);
+    console.log("engine initialized");
+
+	let r = 0;
+	while (r < ROUND)
+	{
+		// 强化 50x2
+		for (let i = 0; i < 2; i++) {
+			await TouchEx("1upgrade.png", { retry: true, delay: 1 });
+			await TouchEx("dots.png", { delay: 1 });
+			await TouchEx("grade_n.png", { delay: 3 });
+			await TouchEx("tick.png", { delay: 2 });
+			await Sleep(1);
+			await TouchEx("upgrade.png");
+			await Sleep(20);
+			await TouchEx("okey.png", { retry: true, delay: 2 });
+			await Sleep(1);
+		}
+
+		// 分解 100
+		await TouchEx("1disenchant.png", { retry: true, delay: 1 });
+		await TouchEx("dots.png", { delay: 1 });		
+		await TouchEx("grade_n.png", { delay: 2 });
+		await TouchEx("tick.png", { delay: 2 });
+		await Sleep(1);
+		await TouchEx("disenchant.png");
+		await Sleep(8);		
+		await Touch(640, 40);
+		await Sleep(2);
+
+		r++;
+		console.log(`progress: ${r*100} / ${ROUND*100}`);
+	}
+	
+	console.log("done");
+})();
